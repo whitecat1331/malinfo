e1f48283d0ad6bff7f2f43df47f9d7bc9f091869
whitecat1331 <whitecat14641@gmail.com>
2023-06-23 22:42:15 -0400

    initial commit

---
+++ malinfo.py
@@ -0,0 +1,55 @@
+class Info:
+    def __init__(self, malware_file):
+        self.malware_file = malware_file
+
+class HashInfo(Info):
+    def __init__(self, malware_file):
+        Info.__init__(self, malware_file)
+        set_md5()
+        set_sha1()
+        set_sha256()
+
+    def set_md5(self):
+        pass
+
+    def set_sha1(self):
+        pass
+
+    def set_sha256(self):
+        pass
+
+class BinaryInfo(Info):
+    def __init__(self, malware_file):
+        Info.__init(self, malware_file)
+        self.set_os()
+        self.set_file_format()
+        self.set_arcitechture()
+
+    def set_os(self):
+        pass
+
+    def set_file_format(self):
+        pass
+
+    def set_arcitechture(self):
+        pass
+
+class ReportInfo(Info):
+    def __init__(self, malware_file):
+        Info.__init__(self, malware_file)
+        self.set_author_name()
+        self.set_date()
+
+    def set_author_name(self):
+        pass
+
+    def set_date(self):
+        pass
+
+class MalInfo:
+    def __init__(self, malware_file):
+        self.malware_file = malware_file
+        self.hash_info = HashInfo(self.malware_file)
+        self.binary_info = BinaryInfo(self.malware_file)
+        self.report_info = ReportInfo(self.malware_file)
+
