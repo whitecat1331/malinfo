636655db4efc90fde5e68f55dcaaa1a6c5082798
whitecat1331 <whitecat14641@gmail.com>
2023-07-25 20:56:47 -0400

    bind results to Monitor type using hashmap

--- monitor.py
+++ monitor.py
@@ -5,9 +5,10 @@ import traceback
 import Proxy.threader
 from multiprocessing import Process, Queue
 from packet_sniffer.core import PacketSniffer
+from abc import ABC, abstractmethod
 
 
-class Monitor:
+class Monitor():
     DURATION = 1
     RESULTS = Queue()
 
@@ -28,7 +29,7 @@ class Monitor:
                         self.tracking_info.append(info)
                 current_time = time.time()
                 run_time = current_time - self.start_time
-        except Exception as e:
+        except Exception:
             traceback.print_exc()
         finally:
             Monitor.RESULTS.put(self.tracking_info)
@@ -40,9 +41,28 @@ class Monitor:
             yield Monitor.RESULTS.get()
 
     @staticmethod
+    def to_dict(monitor_order):
+        results = Monitor.get_all_results()
+        for i in range(len(monitor_order)):
+            yield {f"{monitor_order[i].__name__}": results[i]}
+
+    @staticmethod
     def print_results():
         for result in Monitor.get_results():
             print(result)
+
+    @staticmethod
+    def print_dict_results(monitor_order):
+        for result in Monitor.to_dict(monitor_order):
+            print(result)
+
+    @staticmethod
+    def get_all_results():
+        results = []
+        for result in Monitor.get_results():
+            results.append(result)
+
+        return results
 
 
 class ProcessMonitor:
@@ -71,9 +91,11 @@ MONITORS = Proxy.threader.Threader.get_threads(globals(), "Monitor")
 
 
 def main():
-    thread = Proxy.threader.Threader("monitor", MONITORS)
-    thread.start()
-    Monitor.print_results()
+    while True:
+        thread = Proxy.threader.Threader("monitor", MONITORS)
+        thread.start()
+        Monitor.print_dict_results(MONITORS)
+        time.sleep(1)
 
 
 if __name__ == "__main__":
